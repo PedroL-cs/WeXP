659df8829a617fb73ed841abb440c5259023e316
Pedro L. <pedrolcs30@gmail.com>
2026-09-10 08:45:46 -0300

    Adiciona suporte a rehypeRaw no componente GuideContent para processamento de HTML

--- src/components/GuideContent/index.tsx
+++ src/components/GuideContent/index.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from 'react-markdown';
 import styles from './styles.module.css';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
+import rehypeRaw from 'rehype-raw';
 
 type GuideContentProps = {
   markdown: string;
@@ -10,7 +11,10 @@ type GuideContentProps = {
 function GuideContent({ markdown }: GuideContentProps) {
   return (
     <div className={styles.guideContent}>
-      <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkBreaks]}
+        rehypePlugins={[rehypeRaw]}
+      >
         {markdown}
       </ReactMarkdown>
     </div>
